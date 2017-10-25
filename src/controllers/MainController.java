53cc9c4089c72de626e4c66cba30d7b7f42ac537
Elie ntumba <elientumba@live.fr>
2017-10-25 08:10:43 +0800

    making the controller implement the initializable interface

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,4 +1,20 @@
 package controllers;
 
-public class MainController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable{
+
+
+    /**
+     * where averything will be set up before being used
+     * @param location
+     * @param resources
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
