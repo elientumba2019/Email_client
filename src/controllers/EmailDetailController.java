5c7a2589a2d054878ae1278feaf243d9db629e68
Elie nsampi <elientumba@live.fr>
2017-11-01 10:24:44 +0800

    making the detail controller implementinitializable

--- src/controllers/EmailDetailController.java
+++ src/controllers/EmailDetailController.java
@@ -1,4 +1,22 @@
 package controllers;
 
-public class EmailDetailController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmailDetailController implements Initializable {
+
+
+    /**
+     * takes care of everything that
+     * must be done before having the scene
+     * on screen
+     * @param location
+     * @param resources
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
