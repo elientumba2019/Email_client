0e202c97eef760712440b6074c3b8d01b06ad763
Elie nsampi <elientumba@live.fr>
2017-11-01 11:06:11 +0800

    Creating the view factory class

---
+++ src/views/ViewFactory.java
@@ -0,0 +1,10 @@
+package views;
+
+
+/**
+ * this class job will bw to take care of everything related to the
+ * scenes. things such as getting scenes , returning images. etc
+ */
+public class ViewFactory {
+
+}
